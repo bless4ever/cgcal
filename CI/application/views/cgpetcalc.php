52e5043b6659e8565bed0126c7be7f5e9403f189
Yang Guoshuai <yangguoshuai@ethercap.com>
2015-08-16 02:44:49 +0800

    done

--- CI/application/views/cgpetcalc.php
+++ CI/application/views/cgpetcalc.php
@@ -4,13 +4,13 @@
         <meta charset="utf-8">
 </head>
 <body>
-        <h3>欢迎来到新一代魔力宝贝算档器</h3>
+        <h3>欢迎来到新一代魔力宝贝算档器>CGCal<</h3>
         <h4>感谢魔力百科提供宠物资料数据<br>
         欢迎关注魔力百科更好（看）的算档器！</h4>
         <h4>代码托管在：<a href="https://github.com/yanggs07/cgcal">github</a></h4>
 
-    <form action= "/" method = "post">
-        宠物名称<input type=text id ="petName" name="petName" value="<?php echo $petName?>"><br>
+    <form action= "/" method = "post" >
+        宠物名称<input type=text id ="petName" name="petName" value="<?php echo $petName?>" style=" width:280px" placeholder="输入宠物名称或拼音简称，如‘螳螂’，‘螳’，‘tl’均可"><br>
         <?php
         if ($petSelect != ''){
             echo '<p>选择宠物：';
@@ -22,9 +22,9 @@
             echo '</p>';
         }
         ?>
-        宠物档次<input type=text name="petGrade" value="<?php echo $petGrade?>" ><br>
-        宠物等级<input type=text name="petLv" value="1" readonly="yes"><br>
-        当前数据<input type=text name="petData" value="<?php echo $petData?>"><br>
+        宠物档次<input type=text name="petGrade" value="<?php echo $petGrade?>" style=" width:120px" placeholder="档次将被自动抓取">（暂不支持自定义）<br>
+        宠物等级<input type=text name="petLv" value="1" style=" width:40px"readonly="yes">（暂时只支持1级宠物哦）<br>
+        当前数据<input type=text name="petData" value="<?php echo $petData?>" style=" width:280px" placeholder="输入血魔攻防敏，以空格分开，可加精神回复"><br>
         <input type = "submit" value = "提交" >
         <?php
         if ($petResult != ''){
